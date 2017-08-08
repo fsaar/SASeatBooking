81b5faf7ad608c301d7c69d5346b11db8984658a
Frank Saar <frank.saar@gmail.com>
2017-08-08 18:54:45 +0400

    Initial Commit

---
+++ SASeatBooking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SASeatBooking
+//
+//  Created by Frank Saar on 08/08/2017.
+//  Copyright © 2017 SAMedialabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
